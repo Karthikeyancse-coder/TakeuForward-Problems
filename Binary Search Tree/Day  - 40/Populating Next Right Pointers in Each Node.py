c81ed28ac9d4a03400ef29d6e51719fb4cbb7f50
Karthikeyan M <karthikeyansudha008@gmail.com>
2026-07-10 17:18:29 +0530

    Update Populating Next Right Pointers in Each Node.py

--- Binary Search Tree/Day  - 40/Populating Next Right Pointers in Each Node.py
+++ Binary Search Tree/Day  - 40/Populating Next Right Pointers in Each Node.py
@@ -1,11 +1,14 @@
 from collections import deque
 from typing import Optional,List 
 
+
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val=0, left=None, right=None,next = None):
         self.val = val
         self.left = left
         self.right = right
+        self.next = next
+
         
 class Solution:
 
@@ -26,25 +29,30 @@ class Solution:
         for val in  vals:
             root  = self.Insert(root, val)
         return root 
-    
 
-    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if root is None:
-            return None
-
-        if root.val == val:
-            return root
-        elif val < root.val:
-            return self.searchBST(root.left, val)
-        else:
-            return self.searchBST(root.right, val)
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         
+        def dfs(node):
+            if node is  None or node.left is  None:
+                return 
+            
+            node.left.next = node.right
+
+            if node.next:
+
+                node.right.next = node.next.left
+            
+            dfs(node.left) 
+            dfs(node.right)
+        dfs(root)
+        return root 
+
+
 
 val = [5, 3, 7, 8]
 tree = Solution().bulidBST(val)
-print(Solution().searchBST(tree,3))
+print(Solution().connect(tree))
 
 val = [10,4,6,1,3,2,4]
 tree = Solution().bulidBST(val)
-print(Solution().searchBST(tree,1))
-
+print(Solution().connect(tree))
